b17d076b6b4034e6559d4fc9f2f6ffb8bb2928ad
MobileTest2016 <mobiletest.mt2016@gmail.com>
2022-11-30 08:47:29 +0530

    LeetCode Practice
    Signed-off-by: MobileTest2016 <mobiletest.mt2016@gmail.com>

---
+++ README.md
@@ -0,0 +1,42 @@
+Arrays + Hashing
+------
+
+:green_circle:	 [242. Valid Anagram](https://leetcode.com/problems/valid-anagram/) [<sub><sup> Hash Table, String, Sorting </sup></sub>]
+
+:yellow_circle:  [49. Group Anagrams](https://leetcode.com/problems/group-anagrams/) [<sub><sup> Array, Hash Table, String, Sorting </sup></sub>]
+
+:yellow_circle:  [438. Find All Anagrams in a String](https://leetcode.com/problems/find-all-anagrams-in-a-string/) [<sub><sup> Hash Table, String, Sliding Window </sup></sub>]
+
+:yellow_circle: <picture><img class="emoji" alt="star" height="35" width="35" src="https://github.com/mobiletest2016/leetcode_practice/blob/master/star.png?raw=true"></picture> [347. Top K Frequent Elements](https://leetcode.com/problems/top-k-frequent-elements/) [<sub><sup> Array, Hash Table, Divide and Conquer, Sorting, Heap (Priority Queue), Bucket Sort, Counting, Quickselect </sup></sub>]
+
+:yellow_circle: <picture><img class="emoji" alt="star" height="35" width="35" src="https://github.com/mobiletest2016/leetcode_practice/blob/master/star.png?raw=true"></picture> [692. Top K Frequent Words](https://leetcode.com/problems/top-k-frequent-words/) [<sub><sup> Hash Table, String, Trie, Sorting, Heap (Priority Queue), Bucket Sort, Counting </sup></sub>]
+
+Two Pointer
+------
+
+:yellow_circle:  [11. Container With Most Water](https://leetcode.com/problems/container-with-most-water/) [<sub><sup> Array, Two Pointers, Greedy </sup></sub>]
+
+:red_circle: <picture><img class="emoji" alt="star" height="35" width="35" src="https://github.com/mobiletest2016/leetcode_practice/blob/master/star.png?raw=true"></picture> [42. Trapping Rain Water](https://leetcode.com/problems/trapping-rain-water/) [<sub><sup> Array, Two Pointers, Dynamic Programming, Stack, Monotonic Stack </sup></sub>]
+
+:green_circle:	 [941. Valid Mountain Array](https://leetcode.com/problems/valid-mountain-array/) [<sub><sup> Array </sup></sub>]
+
+:yellow_circle:  [15. 3Sum](https://leetcode.com/problems/3sum/) [<sub><sup> Array, Two Pointers, Sorting </sup></sub>]
+
+:yellow_circle:  [16. 3Sum Closest](https://leetcode.com/problems/3sum-closest/) [<sub><sup> Array, Two Pointers, Sorting </sup></sub>]
+
+
+DFS/BFS
+------
+:yellow_circle:  [1926. Nearest Exit from Entrance in Maze](https://leetcode.com/problems/nearest-exit-from-entrance-in-maze/) [<sub><sup> Array, Breadth-First Search, Matrix </sup></sub>]
+
+:yellow_circle:  [200. Number of Islands](https://leetcode.com/problems/number-of-islands/) [<sub><sup> Array, Depth-First Search, Breadth-First Search, Union Find, Matrix </sup></sub>]
+
+:red_circle:  [329. Longest Increasing Path in a Matrix](https://leetcode.com/problems/longest-increasing-path-in-a-matrix/) [<sub><sup> Array, Dynamic Programming, Depth-First Search, Breadth-First Search, Graph, Topological Sort, Memoization, Matrix </sup></sub>]
+
+
+Uncategorized
+------
+
+:yellow_circle:  [1658. Minimum Operations to Reduce X to Zero](https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/) [<sub><sup> Array, Hash Table, Binary Search, Sliding Window, Prefix Sum </sup></sub>]
+
+:red_circle:  [1770. Maximum Score from Performing Multiplication Operations](https://leetcode.com/problems/maximum-score-from-performing-multiplication-operations/) [<sub><sup> Array, Dynamic Programming </sup></sub>]
---
+++ add_prob.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+if ! command -v jq &> /dev/null
+then
+    echo "jq not be found. Install jq"
+    exit
+fi
+
+if [ $# -eq 0 ]
+  then
+    echo "Usage: $0 LEETCODE_URL"
+    exit 1
+fi
+
+url="$1"
+
+url=$(echo "https${url#*https}")
+echo "Final URL: $url"
+
+if grep -q "$url" README.md; then
+	echo "Problem exists"
+	exit 1
+fi
+
+response=$(curl -i -sS $url)
+if [[ $response == "" ]]; then
+	echo "Could not get valid response from URL: $url..."
+	exit 1
+fi
+
+cookie=$(echo $response | grep set-cookie | cut -f2 -d':' | xargs | cut -f1 -d';')
+csrftoken=$(echo $cookie | cut -f2 -d'=')
+slug=""
+if [[ $url == */ ]]
+then
+	slug=$(echo $url | rev | cut -f2 -d'/' | rev)
+else
+	slug=$(echo $url | rev | cut -f1 -d'/' | rev)
+fi
+
+content=$(curl -sS -X post -H "Content-Type: application/json" -H "referer: $url" -H "cookie: $cookie" -H "x-csrftoken: $csrftoken" --request POST --data '{"operationName":"getQuestionDetail","variables":{"titleSlug":"'$slug'"},"query":"query getQuestionDetail($titleSlug: String!) {\n  isCurrentUserAuthenticated\n  question(titleSlug: $titleSlug) {\n    questionId\n    questionFrontendId\n    questionTitle\n    translatedTitle\n    questionTitleSlug\n    content\n    translatedContent\n    difficulty\n    stats\n    allowDiscuss\n    contributors {\n      username\n      profileUrl\n      __typename\n    }\n    similarQuestions\n    mysqlSchemas\n    randomQuestionUrl\n    sessionId\n    categoryTitle\n    submitUrl\n    interpretUrl\n    codeDefinition\n    sampleTestCase\n    enableTestMode\n    metaData\n    enableRunCode\n    enableSubmit\n    judgerAvailable\n    infoVerified\n    envInfo\n    urlManager\n    article\n    questionDetailUrl\n    libraryUrl\n    adminUrl\n    companyTags {\n      name\n      slug\n      translatedName\n      __typename\n    }\n    companyTagStats\n    topicTags {\n      name\n      slug\n      translatedName\n      __typename\n    }\n    __typename\n  }\n  interviewed {\n    interviewedUrl\n    companies {\n      id\n      name\n      slug\n      __typename\n    }\n    timeOptions {\n      id\n      name\n      __typename\n    }\n    stageOptions {\n      id\n      name\n      __typename\n    }\n    __typename\n  }\n  subscribeUrl\n  isPremium\n  loginUrl\n}\n"}' https://leetcode.com/graphql/)
+id=$(echo $content | jq .data.question.questionFrontendId | tr -d '"')
+title=$(echo $content | jq .data.question.questionTitle | tr -d '"')
+difficulty=$(echo $content | jq .data.question.difficulty | tr -d '"')
+topicTags=$(echo $content | jq .data.question.topicTags | jq '.[].name' | tr -d '"' | tr '\n' ',' | sed 's/,$//g'  | sed 's/,/, /g')
+
+echo "Question details found:"
+echo "id: $id"
+echo "title: $title"
+echo "difficulty: $difficulty"
+echo "topicTags: $topicTags"
+
+if [[ $id == "null" || $title == "null" || $difficulty == "null" ]]; then
+	echo "Could not get some details for the question. Exiting..."
+	exit 1
+fi
+
+echo "Updating README.md"
+if [[ $difficulty == "Medium" ]]; then
+	echo -n ":yellow_circle: " >> README.md
+elif [[ $difficulty == "Hard" ]]; then
+	echo -n ":red_circle: " >> README.md
+else
+	echo -n ":green_circle:	" >> README.md
+fi
+
+read -p "Star this question (y/n)?" choice
+	if [[ $choice == 'y' || $choise == 'Y' ]]; then
+		echo -n  "<picture><img class=\"emoji\" alt=\"star\" height=\"35\" width=\"35\" src=\"https://github.com/mobiletest2016/leetcode_practice/blob/master/star.png?raw=true\"></picture> " >> README.md
+	else
+		echo -n  " " >> README.md
+	fi
+
+echo -n "[$id. $title]($url) " >> README.md
+
+echo "[<sub><sup> $topicTags </sup></sub>]" >> README.md
+
+echo "" >> README.md
+
+echo "Commiting to git"
+git add README.md
+git commit -as -m "$url"
